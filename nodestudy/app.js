7d8a300849941677e8821565055321ab04f4f685
yoonjin0130 <sommylee@gmail.com>
2025-06-23 17:00:05 +0900

    Nodestudy

---
+++ nodestudy/app.js
@@ -0,0 +1,31 @@
+let b = 1
+b = 2
+//console.log(b)
+
+let d = 10
+//console.log(d, typeof d)
+d = '문자'
+//console.log(d, typeof d)
+
+ if(1 < 2) {
+ //   console.log("참")
+ } else {
+ //   console.log("거짓")
+ }
+
+ 
+
+ console.log("콜라, 사이다, 환타 중 하나를 고르세요.")
+ 음료 = '콜라'
+ 금액 = 0
+ if(음료 == '콜라') {
+    console.log('콜라를 주문했습니다.')
+    금액 = 500
+ }else if(음료 == '사이다') {
+    console.log('사이다를 주문했습니다.')
+    금액 = 450
+ }else if(음료 == '환타') {
+    console.log('환타를 주문했습니다.')
+    금액 = 300
+ }
+ console.log(음료 + '의 가격은' + 금액 '입니다.')
