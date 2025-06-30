d1469eb0f2a5e1982b1c004315f8e88b0a5550fc
yoonjin0130 <sommylee@gmail.com>
2025-06-30 11:56:18 +0900

    실습용

--- 파이썬/memo.py
+++ 파이썬/memo.py
@@ -4,8 +4,14 @@ def 메모장생성():
     
     
 def 메모장글쓰기(파일):
-    글 = input('내용을 입력하세요.')
-    파일.write(글)
+    while True:
+        글 = input('내용을 입력하세요.')
+        if 글 == '':
+            글 = '\n'
+        elif 글 == 'exit()':
+            break
+        파일.write(글)
+    
     파일.close()
     
 메모장글쓰기(메모장생성())
